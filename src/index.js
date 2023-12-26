c5a3b438a110f47752bd514baaa7fd0f4ecdda5d
AkbarFikri <akbarfikriabdillah@gmail.com>
2023-12-26 17:16:51 +0700

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
-import express from 'express';
-import router from './router/userRoute.js';
-import cors from 'cors';
+import express from "express";
+import router from "./router/userRoute.js";
+import cors from "cors";
 
 const app = express();
-app.use(cors({
-  origin: '*',
-}))
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use(express.json());
 app.use(router);
 
-app.listen(3000, () => {
-  console.log("Server started!")
-})
+app.listen(443, () => {
+  console.log("Server started!");
+});
